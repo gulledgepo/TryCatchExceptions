effefd1458ee3b10f717f75011fc464c790f609b
gulledgepo <gulledgepo@gmail.com>
2017-09-05 11:34:53 -0700

    Initial Commit

---
+++ Week2Assignment2/Week2Assignment2/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace Week2Assignment2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNumOne = new System.Windows.Forms.TextBox();
+            this.lblMultiply = new System.Windows.Forms.Label();
+            this.txtNumTwo = new System.Windows.Forms.TextBox();
+            this.lstProduct = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lstNumOne = new System.Windows.Forms.ListBox();
+            this.btnMultiply = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.lstNumTwo = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // txtNumOne
+            // 
+            this.txtNumOne.Location = new System.Drawing.Point(12, 21);
+            this.txtNumOne.Name = "txtNumOne";
+            this.txtNumOne.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.txtNumOne.Size = new System.Drawing.Size(71, 20);
+            this.txtNumOne.TabIndex = 0;
+            this.txtNumOne.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // lblMultiply
+            // 
+            this.lblMultiply.AutoSize = true;
+            this.lblMultiply.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMultiply.Location = new System.Drawing.Point(89, 23);
+            this.lblMultiply.Name = "lblMultiply";
+            this.lblMultiply.Size = new System.Drawing.Size(15, 18);
+            this.lblMultiply.TabIndex = 1;
+            this.lblMultiply.Text = "*";
+            // 
+            // txtNumTwo
+            // 
+            this.txtNumTwo.Location = new System.Drawing.Point(110, 21);
+            this.txtNumTwo.Name = "txtNumTwo";
+            this.txtNumTwo.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.txtNumTwo.Size = new System.Drawing.Size(71, 20);
+            this.txtNumTwo.TabIndex = 2;
+            this.txtNumTwo.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // lstProduct
+            // 
+            this.lstProduct.FormattingEnabled = true;
+            this.lstProduct.Location = new System.Drawing.Point(214, 51);
+            this.lstProduct.Name = "lstProduct";
+            this.lstProduct.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lstProduct.Size = new System.Drawing.Size(76, 212);
+            this.lstProduct.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(187, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 18);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "=";
+            // 
+            // lstNumOne
+            // 
+            this.lstNumOne.FormattingEnabled = true;
+            this.lstNumOne.Location = new System.Drawing.Point(13, 51);
+            this.lstNumOne.Name = "lstNumOne";
+            this.lstNumOne.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lstNumOne.Size = new System.Drawing.Size(70, 212);
+            this.lstNumOne.TabIndex = 5;
+            // 
+            // btnMultiply
+            // 
+            this.btnMultiply.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMultiply.Location = new System.Drawing.Point(296, 9);
+            this.btnMultiply.Name = "btnMultiply";
+            this.btnMultiply.Size = new System.Drawing.Size(75, 32);
+            this.btnMultiply.TabIndex = 6;
+            this.btnMultiply.Text = "Multiply";
+            this.btnMultiply.UseVisualStyleBackColor = true;
+            this.btnMultiply.Click += new System.EventHandler(this.btnMultiply_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(296, 51);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 32);
+            this.btnClear.TabIndex = 7;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // lstNumTwo
+            // 
+            this.lstNumTwo.FormattingEnabled = true;
+            this.lstNumTwo.Location = new System.Drawing.Point(111, 51);
+            this.lstNumTwo.Name = "lstNumTwo";
+            this.lstNumTwo.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lstNumTwo.Size = new System.Drawing.Size(70, 212);
+            this.lstNumTwo.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(386, 278);
+            this.Controls.Add(this.lstNumTwo);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnMultiply);
+            this.Controls.Add(this.lstNumOne);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstProduct);
+            this.Controls.Add(this.txtNumTwo);
+            this.Controls.Add(this.lblMultiply);
+            this.Controls.Add(this.txtNumOne);
+            this.Name = "Form1";
+            this.Text = "Exercise 1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNumOne;
+        private System.Windows.Forms.Label lblMultiply;
+        private System.Windows.Forms.TextBox txtNumTwo;
+        private System.Windows.Forms.ListBox lstProduct;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lstNumOne;
+        private System.Windows.Forms.Button btnMultiply;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.ListBox lstNumTwo;
+    }
+}
+
---
+++ Week2Assignment2/Week2Assignment2/Form1.cs
@@ -0,0 +1,97 @@
+﻿//Week 1 Assignment 1
+//Chapter 12 Exercise 1, pg 834
+//08/18/2017
+//Program gets two values user input values, stores them, multiplies them
+//and displays the product in a list box as well as both numbers and the product
+//in a separate listbox. Emphasis is on exceptions for the try-catch block.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Week2Assignment2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        public void ClearForms()
+        {
+            //Clear all forms called upon
+            txtNumOne.Clear();
+            txtNumTwo.Clear();
+            lstNumOne.Items.Clear();
+            lstNumTwo.Items.Clear();
+            lstProduct.Items.Clear();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            //Call our Clear Forms
+            ClearForms();
+        }
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+            //Create our three int variables
+            int numberOne = 0;
+            int numberTwo = 0;
+            int product;
+            //Our big try clause to make sure we can accomplish everything, with two exceptions to inform the user
+            //of what went wrong
+            try
+            {
+               //If our text boxes are null or empty we throw a new exception which will warn the user to fill out all fields
+               if ((txtNumOne.Text == "" || txtNumOne.Text == null) || (txtNumTwo.Text == "" || txtNumTwo.Text == null)) throw new Exception();
+               //We try to parse the two fields into int variables, if this can't happen it will trigger our FormatException catch
+               numberOne = int.Parse(txtNumOne.Text);
+               numberTwo = int.Parse(txtNumTwo.Text);
+               //Set our product variable to a method that multiplies numberOne and numberTwo
+               product = Multiply(numberOne, numberTwo);
+               //We clear our the text fields
+               txtNumOne.Clear();
+               txtNumTwo.Clear();
+               txtNumOne.Focus();
+               lstProduct.Items.Add(product.ToString());
+               lstNumOne.Items.Add(numberOne.ToString());
+               lstNumTwo.Items.Add(numberTwo.ToString());
+               txtNumOne.Focus();
+            }
+            catch (IndexOutOfRangeException exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+            //This catch runs when a non whole number or non-numeric character is entered
+            catch (FormatException)
+            {
+               MessageBox.Show("Please ensure only whole numbers are entered in the fields.");
+            }
+            //This catch runs when a null or empty space is provided           
+            catch (Exception)
+            {
+                MessageBox.Show("Please ensure all fields are filled out.");
+            }
+
+        }
+
+        public int Multiply(int numOne, int numTwo)
+        {
+            return numOne * numTwo;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //On load put the focus into text box number One
+            txtNumOne.Focus();
+        }
+    }
+}
